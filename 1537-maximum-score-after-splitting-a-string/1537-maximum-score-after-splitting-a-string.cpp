ce8e57ee9901b8a97f93caaae7a8d70c05b261d8
AuroBhuyan <145313027+AuroBhuyan@users.noreply.github.com>
2025-01-01 17:38:55 +0530

    Time: 0 ms (100%), Space: 8.8 MB (38.26%) - LeetHub

--- 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.cpp
+++ 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.cpp
@@ -1,37 +1,21 @@
 class Solution {
 public:
-    string S;
-    int countzero(int l,int h){
-        int c=0;
-        for(int i=l;i<=h;i++){
-           if(S[i]=='0')
-           c++;
-        }
-
-        return c;
-    }
-
-    int countone(int l,int h){
-        int c=0;
-        for(int i=l;i<=h;i++){
-           if(S[i]=='1')
-           c++;
-        }
-
-        return c;
-    }
     
     int maxScore(string s) {
-        S=s;
         int l = s.size();
+        int onec = count(s.begin(),s.end(),'1'); //Prefix sum
+        int zeroc = 0;
         
-        int m=0;
-        int max =0;
-        while(m<l-1){
-         int temp = countzero(0,m)+countone(m+1,l-1);
-         if(temp>max) max=temp;
+        
+        int max = 0;
+        for(int i=0;i<l-1;i++){
+            if(s[i]=='0')
+            zeroc++;
+            else
+            onec--;
 
-         m++;
+            int temp = zeroc+onec;
+            if(temp>max) max=temp;
         }
 
         return max;
