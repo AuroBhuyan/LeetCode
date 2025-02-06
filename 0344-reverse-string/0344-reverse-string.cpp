aa8289bb745dd01a9e6f6a17bc29e9e2c91cc770
AuroBhuyan <145313027+AuroBhuyan@users.noreply.github.com>
2025-02-06 13:46:54 +0530

    Time: 0 ms (100%), Space: 27.3 MB (51.99%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int l = s.size();
+        int i = 0;
+        int j = l-1;
+        while(i<j){
+            swap(s[i],s[j]);
+            i++;
+            j--;
+        }
+    }
+};
