297dc0ba75639d07a3330a672034c17b8e44a19b
AuroBhuyan <145313027+AuroBhuyan@users.noreply.github.com>
2025-09-16 17:01:48 +0530

    Time: 0 ms (100%), Space: 9.8 MB (10.61%) - LeetHub

---
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> pas(numRows);
+
+        for(int i=0;i<numRows;i++){
+            pas[i] = vector<int>(i+1);
+        }
+
+        for(int i=0;i<numRows;i++){
+            pas[i][0] = 1;
+            pas[i][i] = 1;
+            for(int j=1;j<i;j++){
+                pas[i][j] = pas[i-1][j-1]+pas[i-1][j];
+            }
+        }
+
+        return pas;
+    }
+};
