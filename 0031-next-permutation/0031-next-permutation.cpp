dc41ce67a7e8e2333a7a9ab690746a1f8f0b0238
AuroBhuyan <145313027+AuroBhuyan@users.noreply.github.com>
2025-09-18 23:47:41 +0530

    Time: 0 ms (100%), Space: 15.6 MB (67.78%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -3,25 +3,26 @@ public:
     void nextPermutation(vector<int>& nums) {
         int length = nums.size();
         int pivot = -1;
-
-        for (int i = length - 2; i >= 0; i--) {
-            if (nums[i] < nums[i + 1]) {
+        
+        for(int i=length-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
                 pivot = i;
                 break;
             }
         }
 
-        if (pivot == -1) {
-            reverse(nums.begin(), nums.end());
-        } else {
-            for (int i = length - 1; i >= 0; i--) {
-                if (nums[i] > nums[pivot]){
-                    swap(nums[i], nums[pivot]);
+        if(pivot==-1) {
+            reverse(nums.begin(),nums.end());
+        }else{
+            for(int i=length-1;i>=0;i--){
+                if(nums[i]>nums[pivot]){
+                    swap(nums[i],nums[pivot]);
                     break;
                 }
             }
-
-            reverse(nums.begin() + pivot + 1, nums.end());
+            reverse(nums.begin()+pivot+1,nums.end());
         }
+
+        
     }
 };
